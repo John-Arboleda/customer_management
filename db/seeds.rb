7376fad0e866ac98ba909a9deed44d1253320ca7
John-Arboleda <johnjack0126@gmail.com>
2023-04-15 23:47:06 -0500

    Create predefined data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Client.create(client_id: 1, name: 'Pedro Pérez', city_id: 1)
+Client.create(client_id: 2, name: 'Ana Álvarez', city_id: 2)
+
+City.create(city_id: 1, name: 'Bogotá')
+City.create(city_id: 2, name: 'Medellín')
+
+User.create(name: 'admin', pass: 'password', email: 'admin@example.com')
